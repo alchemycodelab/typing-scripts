626b22870526d9727f0d4b5fd1e8450b198551c5
Lance Owen Merrill <40847460+Zilula@users.noreply.github.com>
2019-04-19 09:14:09 -0700

    Created functionExamples.js

---
+++ scripts/functionExamples.js
@@ -0,0 +1,41 @@
+// Return the string Hello World
+function helloWorld() {
+    return 'Hello World';
+}
+helloWorld();
+
+// Returns the opposite of a number
+function findTheOppositeNumber(someNum) {
+    return someNum * -1;
+
+}
+findTheOppositeNumber(10);
+findTheOppositeNumber(-48);
+
+// Square a number
+function squareANumber(number) {
+    return Math.pow(number, 2);
+}
+squareANumber(5);
+squareANumber(8);
+
+// Return the sum of two numbers
+function addTwoNumbers(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+addTwoNumbers(10, 23);
+addTwoNumbers(9, 3);
+
+// Return the difference between to numbers 
+function subtractANumber(total, number) {
+    return total - number;
+}
+subtractANumber(100, 40);
+subtractANumber(24, 12);
+
+function doSomeMath(numOne, numTwo, numThree) {
+    return (numOne * numTwo) - numThree;
+}
+doSomeMath(10, 3, 5);
+doSomeMath(8, 5, 9);
+doSomeMath(1000, 42, 196);
