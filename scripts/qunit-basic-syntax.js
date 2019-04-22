003d717793249dd5ce794a26a650a5fc611c7a8c
martypdx <marty@martypdx.com>
2019-04-22 14:32:44 -0700

    convert qunit to arrow function for callbacks

--- scripts/qunit-basic-syntax.js
+++ scripts/qunit-basic-syntax.js
@@ -1,14 +1,16 @@
 const test = QUnit.test;
 
-test('that true equals true', function(assert) {
+test('that true equals true', (assert) => {
     assert.equal(true, true);
 });
 
-test('that two strings are equal', function(assert) {
-    assert.equal('banana', 'banana');
+test('that two strings are equal', (assert) => {
+    const expected = 'banana';
+    const actual = 'banana';
+    assert.equal(actual, expected);
 });
 
-// this test should fail
-test('that true equals false', function(assert) {
+// this test should fail!
+test('that true equals false', (assert) => {
     assert.equal(true, false);
 });
